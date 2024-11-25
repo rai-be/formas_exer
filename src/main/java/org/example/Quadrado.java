c7d5dc85833d0426fe13f92146855847f98b0d6f
raiani <raiani.guimaraes@zup.com.br>
2024-11-25 15:20:30 -0300

    feat: creates Square class with area calculation

---
+++ src/main/java/org/example/Quadrado.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Quadrado implements FormaGeometrica {
+    private double lado;
+
+    public Quadrado(double lado) {
+        this.lado = lado;
+    }
+
+    @Override
+    public double calcularArea() {
+        return lado * lado;
+    }
+}
