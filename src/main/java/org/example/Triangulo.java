41f012f730a732068db588ef7a52d9ad663aaf3f
raiani <raiani.guimaraes@zup.com.br>
2024-11-25 15:29:47 -0300

    feat: creates Triangle class with area calculation

---
+++ src/main/java/org/example/Triangulo.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Triangulo implements FormaGeometrica {
+    private double base;
+    private double altura;
+
+    public Triangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    @Override
+    public double calcularArea() {
+        return (base * altura) / 2;
+    }
+}
